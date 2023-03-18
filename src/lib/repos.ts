62e4315cc8476bbba82531c7482f73b9d1b83085
Benjamín Vicente <benjavicente@uc.cl>
2023-03-18 18:12:11 -0300

    add background

--- src/lib/repos.ts
+++ src/lib/repos.ts
@@ -4,7 +4,7 @@ const graphqlUrl = "https://api.github.com/graphql";
 const graphqlImgQuery = "query($user:String!,$repo:String!){repository(owner:$user,name:$repo){openGraphImageUrl}}";
 const headers = { "Content-Type": "application/json", Authorization: `Bearer ${import.meta.env.GITHUB_TOKEN}` };
 
-export async function getRepoImg(variables: { user: string; repo: string }): Promise<string | undefined> {
+export async function getRepoImg(variables: { user: string; repo: string }) {
 	const cached = cache.get(`${variables.user}/${variables.repo}/img`);
 	const body = JSON.stringify({ query: graphqlImgQuery, variables });
 	if (cached) return cached;
@@ -13,8 +13,12 @@ export async function getRepoImg(variables: { user: string; repo: string }): Pro
 	if (!response.ok) throw new Error(await response.text());
 
 	const json = await response.json();
-	const imgUrl = json.data.repository.openGraphImageUrl;
-	cache.put(`${variables.user}/${variables.repo}/img`, imgUrl, 5 * 1000);
+	const imgUrl = json.data.repository.openGraphImageUrl as string;
 
-	return imgUrl;
+	const hasOpenGraphImage = imgUrl.includes("repository-images.githubusercontent.com/");
+	const realImageUrl = hasOpenGraphImage ? imgUrl : undefined;
+
+	cache.put(`${variables.user}/${variables.repo}/img`, realImageUrl, 5 * 1000);
+
+	return realImageUrl;
 }
