f358f1d74d9553f504f5603f3250b9f2a3f7ef96
Lucas Natero <64765306+lnatero@users.noreply.github.com>
2023-07-18 23:17:25 -0400

    Update +page.svelte

--- src/routes/join/internal/+page.svelte
+++ src/routes/join/internal/+page.svelte
@@ -4,11 +4,11 @@
 	<p>
 		Para potenciar e impulsar la comunidad de código abierto en la universidad, trabajamos en
 		conjunto para organizar actividades, talleres, y proyectos. El equipo interno se podría pensar
-		como el “equipo de coordinación”, pero es más que eso: somos un grupo motivado que busca
+		como el “equipo de coordinación”, pero es más que eso; somos un grupo motivado que busca
 		colaborar e impulsar a los demás.
 	</p>
 
-	<h2>Que es lo que buscamos</h2>
+	<h2>¿Qué es lo que buscamos?</h2>
 
 	<ul>
 		<li>Tienes iniciativa, puedes identificar oportunidades y liderar su ejecución.</li>
@@ -16,7 +16,7 @@
 		<li>Sabes colaborar y comunicarte bien con los demás.</li>
 	</ul>
 
-	<h2>Como funciona</h2>
+	<h2>¿Cómo funciona?</h2>
 
 	<p>
 		Para lograr bien nuestros objetivos, nos dividimos en 3 grupos, cada uno con objetivos
@@ -36,18 +36,18 @@
 	</ul>
 	<p>Para este grupo en específico buscamos:</p>
 	<ul>
-		<li>Tienes que saber programar, tienes cuenta de GitHub.</li>
+		<li>Tienes que saber programar, tienes que tener cuenta de GitHub.</li>
 		<li>Tines una idea de como se planifican los proyectos de software.</li>
 		<li>Un gran plus es si tienes experiencia en proyectos reales (experiencia laboral).</li>
 	</ul>
 	<p>
 		Aunque este grupo sea “el lado técnico”, también son importantes las habilidades blandas, como
-		por ejemplo, cuando se forman los equipos de desarrollo.
+		por ejemplo, para cuando se forman los equipos de desarrollo.
 	</p>
 
-	<h3>🎺 Grupo Comunicaciones y Diseño</h3>
+	<h3>🎨 Grupo Comunicaciones y Diseño</h3>
 	<p>
-		Se encarga de RRSS y guiar el diseño, buscando darle visibilidad de la organización y a sus
+		Se encarga de RRSS y guiar el diseño de la organización, buscando darle visibilidad tanto a OSUC como a sus
 		proyectos. Sus tareas principales son:
 	</p>
 	<ul>
@@ -59,6 +59,24 @@
 	<p>Para esto buscamos:</p>
 	<ul>
 		<li>Te manejas con herramientas de diseño (usamos principalmente Figma).</li>
-		<li />
+		<li>Tener experiencia en diseño gráfico (idealmente).<li/>
+		<li>Sepas no tomar críticas como un ataque personal.<li/>
+	</ul>
+
+	<h3>📠 Grupo Administrativo (?)</h3>
+	<p>
+		Se encarga del funcionamiento y la representación "legal" de la organización.
+	</p>
+	<ul>
+		<li>Asegurarse de que se mantenga la visión y la misión de la organización.</li>
+		<li>Representar a OSUC frente la universidad y otras entidades.</li>
+		<li>Llevar a cabo las votaciones de los cargos de coordinación.</li>
+		<li>Mantener el contacto con sponsors y administrar las finanzas de la organización.</li>
+	</ul>
+	<p>Para esto buscamos:</p>
+	<ul>
+		<li>Seas una persona responable y organizada.</li>
+		<li>Que puedas entablar conversaciones con personas que no conoces.<li/>
+		<li>Que seas capaz de liderar la organización y asegurar su buen funcionamiento durante el semestre.<li/>
 	</ul>
 </div>
