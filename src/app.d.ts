8d9a4f4ec7e98b7bd32792341bffa81606fec0b2
Benjamín Vicente <62021328+benjavicente@users.noreply.github.com>
2023-07-29 16:18:37 -0400

    create meta tags (#11)

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,19 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
+	interface PageMetadata {
+		title?: string;
+		description?: string;
+		twitterImgUrl?: string;
+		openGraphImgUrl?: string;
+	}
+
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		interface PageData {
+			meta: PageMetadata;
+		}
 		// interface Platform {}
 	}
 }
