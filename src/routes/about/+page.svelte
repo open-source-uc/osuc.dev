6c2547712c8d9f3f76dd97cea38f8b3e27b4f083
Benjamín Vicente <benjavicente@uc.cl>
2023-07-30 22:16:23 -0400

    docs: initial history draft

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,33 @@
-<div class="h-full text-center flex justify-center items-center font-semibold text-base-400">
-	Aquí iria más sobre nosotros si hubiera más tiempo para hacer esta página
+<div class="container-sm my-8 prose-headings:font-display">
+	<main class="prose">
+		<h1>Sobre Open Source UC<span class="text-primary-500">.</span></h1>
+		<p>
+			Open Source UC es una iniciativa estudiantil que busca crear y potenciar proyectos de código
+			abierto en la Pontificia Universidad Católica de Chile.
+		</p>
+		<p>
+			La iniciativa es llamada abreviadamente como OSUC, y en ocasiones se usa Open Source eUC para
+			denotar que es de estudiantes de la UC.
+		</p>
+
+		<h2>Porqué Open Source en la Universidad</h2>
+		<ul>
+			<li>
+				Cualquier persona puede colaborar y aprender en los proyectos. Evita que varios tengan que
+				reinventar la rueda, y da el espacio para que se puedan crear proyectos grandes en conjunto.
+			</li>
+			<li>
+				Crea un espacio de aprendizaje y ayuda. Uno puede ir más allá de los cursos de la
+				universidad, aprendiendo y aplicando conocimientos en proyectos reales.
+			</li>
+		</ul>
+
+		<h2>Historia</h2>
+		<p>
+			Desde el 2006 existieron grupos como OpenPUC que buscaban incentivar el interés por el código
+			abierto en la universidad. El 2017 se armó un grupo que creó Open Source UC que se mantuvo
+			activo hasta finales del 2018. Y el 2021 se reorganizó la iniciativa, quedando en la
+			iniciativa que conocemos ahora.
+		</p>
+	</main>
 </div>
