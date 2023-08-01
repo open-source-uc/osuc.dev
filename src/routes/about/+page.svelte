8ad083d8e6ca7836fb074238cca4f235372e1fe3
Benjamín Vicente <62021328+benjavicente@users.noreply.github.com>
2023-08-01 09:46:27 -0400

    About page (#12)

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,50 @@
-<div class="h-full text-center flex justify-center items-center font-semibold text-base-400">
-	Aquí iria más sobre nosotros si hubiera más tiempo para hacer esta página
+<div class="container-sm my-8 prose-headings:font-display">
+	<main class="prose">
+		<h1>Sobre Open Source UC<span class="text-primary-500">.</span></h1>
+		<p>
+			Open Source UC es una iniciativa estudiantil que busca crear y potenciar proyectos de código
+			abierto en la Pontificia Universidad Católica de Chile.
+		</p>
+		<p>
+			La iniciativa es llamada abreviadamente como OSUC, y en ocasiones se usa Open Source eUC para
+			denotar que es de estudiantes de la UC.
+		</p>
+
+		<h2>Porqué Open Source en la Universidad</h2>
+		<ul>
+			<li>
+				Cualquier persona puede colaborar y aprender en los proyectos. Evita que varios tengan que
+				reinventar la rueda, y da el espacio para que se puedan crear proyectos grandes en conjunto.
+			</li>
+			<li>
+				Crea un espacio de aprendizaje y ayuda. Uno puede ir más allá de los cursos de la
+				universidad, aprendiendo y aplicando conocimientos en proyectos reales.
+			</li>
+			<li>
+				Se potencia una comunidad donde se comparten experiencias y motivaciones. Al desarrollar de
+				forma más pública, podrás conocer gente con mismos intereses o que pueda guiarte en tu
+				aprendizaje.
+			</li>
+		</ul>
+
+		<h2>Qué es lo que hacemos</h2>
+		<p>
+			Organizamos, potenciamos y mantenemos proyectos de código abierto. Nos enfocamos en proyectos
+			relacionados a una problemática que identificamos en la universidad, aunque también apoyamos
+			otros que sean de interés.
+		</p>
+		<p>
+			Puedes ver los proyectos en <a href="https://github,com/open-source-uc">nuestro GitHub</a> y
+			se participa cada semestre en la
+			<a href="/join">página de como unirse</a>.
+		</p>
+
+		<h2>Historia</h2>
+		<p>
+			Desde el 2006 existieron grupos como OpenPUC que buscaban incentivar el interés por el código
+			abierto en la universidad. El 2017 se armó un grupo que creó Open Source UC que se mantuvo
+			activo hasta finales del 2018. Y el 2021 se reorganizó la iniciativa, quedando en la
+			organización que conocemos ahora.
+		</p>
+	</main>
 </div>
