c49c77db5878ef1142bac6828dfe83fd56de7adb
Benjamín Vicente <benjavicente@uc.cl>
2023-08-01 09:46:09 -0400

    more stuff

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -20,14 +20,31 @@
 				Crea un espacio de aprendizaje y ayuda. Uno puede ir más allá de los cursos de la
 				universidad, aprendiendo y aplicando conocimientos en proyectos reales.
 			</li>
+			<li>
+				Se potencia una comunidad donde se comparten experiencias y motivaciones. Al desarrollar de
+				forma más pública, podrás conocer gente con mismos intereses o que pueda guiarte en tu
+				aprendizaje.
+			</li>
 		</ul>
+
+		<h2>Qué es lo que hacemos</h2>
+		<p>
+			Organizamos, potenciamos y mantenemos proyectos de código abierto. Nos enfocamos en proyectos
+			relacionados a una problemática que identificamos en la universidad, aunque también apoyamos
+			otros que sean de interés.
+		</p>
+		<p>
+			Puedes ver los proyectos en <a href="https://github,com/open-source-uc">nuestro GitHub</a> y
+			se participa cada semestre en la
+			<a href="/join">página de como unirse</a>.
+		</p>
 
 		<h2>Historia</h2>
 		<p>
 			Desde el 2006 existieron grupos como OpenPUC que buscaban incentivar el interés por el código
 			abierto en la universidad. El 2017 se armó un grupo que creó Open Source UC que se mantuvo
 			activo hasta finales del 2018. Y el 2021 se reorganizó la iniciativa, quedando en la
-			iniciativa que conocemos ahora.
+			organización que conocemos ahora.
 		</p>
 	</main>
 </div>
