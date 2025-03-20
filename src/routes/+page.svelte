e08ffd6c5d0d6f1a56d7be71bf90c35071d96db4
Utmite <rockimunoz94@gmail.com>
2025-03-20 13:57:03 -0300

    feat(ancor telegram)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -61,7 +61,7 @@
 		{/each}
 	</ul>
 	<div
-		class="flex flex-col sm:max-w-none max-w-xs mx-auto text-center sm:flex-row justify-center my-8 gap-2"
+		class="mx-auto grid w-fit auto-rows-auto grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 pt-6"
 	>
 		<a
 			href="https://github.com/issues?q=is:open+is:issue+archived:false+user:open-source-uc"
@@ -86,6 +86,18 @@
 				/></svg
 			>
 			Ir a GitHub
+		</a>
+		<a
+			href="https://t.me/+zQdotSmFEIRkMzcx"
+			class="flex items-center justify-center bg-primary-600 text-base-100 gap-2 px-4 py-2 rounded hover:underline"
+		>
+			<svg xmlns="http://www.w3.org/2000/svg" class="h-6" viewBox="0 0 24 24"
+				><path
+					fill="currentColor"
+					d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm4.64 6.8c-.15 1.58-.8 5.42-1.13 7.19c-.14.75-.42 1-.68 1.03c-.58.05-1.02-.38-1.58-.75c-.88-.58-1.38-.94-2.23-1.5c-.99-.65-.35-1.01.22-1.59c.15-.15 2.71-2.48 2.76-2.69a.2.2 0 0 0-.05-.18c-.06-.05-.14-.03-.21-.02c-.09.02-1.49.95-4.22 2.79c-.4.27-.76.41-1.08.4c-.36-.01-1.04-.2-1.55-.37c-.63-.2-1.12-.31-1.08-.66c.02-.18.27-.36.74-.55c2.92-1.27 4.86-2.11 5.83-2.51c2.78-1.16 3.35-1.36 3.73-1.36c.08 0 .27.02.39.12c.1.08.13.19.14.27c-.01.06.01.24 0 .38z"
+				/></svg
+			>
+			Grupo de Telegram
 		</a>
 	</div>
 </section>
