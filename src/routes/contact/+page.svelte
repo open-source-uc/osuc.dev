e4c997a8dc49e69033ad6d160fcc06c7da51155c
Utmite <rockimunoz94@gmail.com>
2025-03-20 11:04:10 -0300

    fix(loading)

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,18 +1,25 @@
+<script>
+	let loading = true;
+
+	function handleLoad() {
+		loading = false;
+	}
+</script>
+
 <div class="prose p-4 prose-headings:font-display w-full h-full">
 	<h1>Contacto</h1>
+
+	{#if loading}
+		<p class="text-xl text-center mb-4">Cargando formulario...</p>
+	{/if}
 	<div class="relative w-full h-[600px]">
-		<div
-			id="loading-message"
-			class="absolute inset-0 flex items-center justify-center bg-gray-50 rounded-lg text-2xl"
-		>
-			Cargando formulario...
-		</div>
 		<iframe
 			class="airtable-embed absolute inset-0 w-full h-full border border-gray-200 rounded-lg"
 			src="https://airtable.com/embed/apprK49P0NhkU8q3c/pagzGz0PrFAGw3rBB/form"
 			frameborder="0"
 			title="Formulario de contacto"
 			allowfullscreen
+			on:load={handleLoad}
 		/>
 		<noscript>
 			<div
--- src/routes/join/form/+page.svelte
+++ src/routes/join/form/+page.svelte
@@ -1,26 +1,33 @@
+<script>
+	let loading = true;
+
+	function handleLoad() {
+		loading = false;
+	}
+</script>
+
 <div class="prose p-4 prose-headings:font-display w-full h-full">
 	<h1>Únete a la comunidad</h1>
+
+	{#if loading}
+		<p class="text-xl text-center mb-4">Cargando formulario...</p>
+	{/if}
+
 	<div class="relative w-full h-[600px]">
-		<div
-			id="loading-message"
-			class="absolute inset-0 flex items-center justify-center bg-gray-50 rounded-lg text-2xl"
-		>
-			Cargando formulario...
-		</div>
 		<iframe
 			class="airtable-embed absolute inset-0 w-full h-full border border-gray-200 rounded-lg"
 			src="https://airtable.com/embed/app7Lzkf0EjHJvuXp/pagzGz0PrFAGw3rBB/form"
 			frameborder="0"
 			allowfullscreen
 			title="Formulario de inscripción"
+			on:load={handleLoad}
 		/>
-		<noscript>
-			<div
-				class="absolute inset-0 flex items-center justify-center bg-gray-50 rounded-lg border border-gray-200 p-4 text-center"
-			>
-				Para ver el formulario de registro, por favor habilita JavaScript en tu navegador o utiliza
-				otro navegador compatible.
-			</div>
-		</noscript>
 	</div>
+
+	<noscript>
+		<div class="mt-4 p-4 text-center bg-gray-50 border border-gray-200 rounded-lg">
+			Para ver el formulario de registro, por favor habilita JavaScript en tu navegador o utiliza
+			otro navegador compatible.
+		</div>
+	</noscript>
 </div>
