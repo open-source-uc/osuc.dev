8c4df7f917dc20df6916227ee4fef5a50cd6c9f6
Utmite <rockimunoz94@gmail.com>
2025-03-20 11:13:48 -0300

    fix(join)

--- src/routes/join/+page.svelte
+++ src/routes/join/+page.svelte
@@ -15,27 +15,28 @@
 		conocimiento sobre desarrollo de software.
 	</p>
 	<p>
-		Para esto, necesitamos personas motivadas que nos ayuden. Tenemos 2 formas en las que te puedes
-		sumar:
+		Para esto, necesitamos personas motivadas que nos ayuden. Tenemos dos formas en las que te
+		puedes sumar:
 	</p>
 
 	<h2>Equipos de Desarrollo</h2>
 	<p>
-		Únete como desarrollador en uno de los proyectos del semestre, dedicándole al menos 4 horas de
-		trabajo a la semana y reuniéndote con tu equipo cada semana. Recibirás ayuda y feedback durante
-		todo el proceso para que puedas aprender cómo desarrollar software en equipo.
+		Únete como desarrollador en uno de los proyectos del semestre, dedicándole tiempo de trabajo a
+		la semana y reuniéndote con tu equipo periódicamente. Recibirás ayuda y feedback durante todo el
+		proceso para que puedas aprender cómo desarrollar software en equipo.
 	</p>
 
 	<h2>Equipo Interno</h2>
 	<p>
 		Apoya en la organización de actividades y proyectos, potenciando la comunidad de código abierto
-		en la universidad. Apoya a los equipos de desarrollo y mantención de proyectos, la difusión de
-		actividades y proyectos, o la organización de diferentes actividades. Se divide en 3 grupos:
+		en la universidad. Ayuda a los equipos de desarrollo y mantenimiento de proyectos, la difusión
+		de actividades y la organización de diferentes eventos. Se divide en:
 	</p>
 	<ul>
-		<li>Proyectos: organiza los equipos de desarrollo y mantiene proyectos</li>
-		<li>Marketing y Diseño: ve la difusión y apoya en el diseño de los proyectos</li>
+		<li>Proyectos: organiza los equipos de desarrollo y mantiene proyectos.</li>
+		<li>Marketing y Diseño: se encarga de la difusión y apoya en el diseño de los proyectos.</li>
 	</ul>
+
 	<div class="not-prose flex items-center gap-2">
 		<input type="checkbox" bind:checked class="w-5 h-5" id="joinCheckbox" />
 		<label for="joinCheckbox" class="text-primary-500 cursor-pointer">
