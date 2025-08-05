2b767de4f8dac37a93647f98b35dfcb85207e6aa
Ignacio Palma <81819758+IgnacioPalma@users.noreply.github.com>
2025-08-04 21:33:28 -0400

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,49 +7,29 @@
 	<main class="prose">
 		<h1>Sobre Open Source UC<span class="text-primary-500">.</span></h1>
 		<p>
-			Open Source UC es una iniciativa estudiantil que busca crear y potenciar proyectos de código
-			abierto en la Pontificia Universidad Católica de Chile.
-		</p>
-		<p>
-			La iniciativa es llamada abreviadamente como OSUC, y en ocasiones se usa Open Source eUC para
-			denotar que es de estudiantes de la UC.
+			Open Source UC es una iniciativa estudiantil de innovación tecnológica que impulsar a estudiantes a colaborar y crecer profesionalmente a través
+			de la filosofía open-source en la Pontificia Universidad Católica de Chile.
 		</p>
 
-		<h2>Por qué Open Source en la Universidad</h2>
+		<h2>¿Por qué existimos?</h2>
 		<ul>
 			<li>
-				Cualquier persona puede colaborar y aprender en los proyectos. Evita que varios tengan que
-				reinventar la rueda, y da el espacio para que se puedan crear proyectos grandes en conjunto.
+				Creamos un espacio de aprendizaje práctico que permite a nuestros miembros ir más allá de los cursos universitarios, aplicando conocimientos en proyectos con impacto real y desarrollando habilidades que demanda la industria tecnológica.
 			</li>
 			<li>
-				Crea un espacio de aprendizaje y ayuda. Uno puede ir más allá de los cursos de la
-				universidad, aprendiendo y aplicando conocimientos en proyectos reales.
+				Construimos una comunidad de crecimiento mutuo donde se comparten experiencias, conocimientos y motivaciones. Al desarrollar de forma pública, Open Source reúne a estudiantes con diversos niveles de experiencia, fomentando el mentoring y la colaboración entre pares.
 			</li>
 			<li>
-				Se potencia una comunidad donde se comparten experiencias y motivaciones. Al desarrollar de
-				forma más pública, podrás conocer gente con mismos intereses o que pueda guiarte en tu
-				aprendizaje.
+				Promovemos la transparencia y el aprendizaje abierto mediante proyectos de código abierto que no solo benefician a nuestra comunidad, sino que también contribuyen al ecosistema tecnológico regional y permiten que otros aprendan de nuestro trabajo.
 			</li>
 		</ul>
 
-		<h2>Qué es lo que hacemos</h2>
-		<p>
-			Organizamos, potenciamos y mantenemos proyectos de código abierto. Nos enfocamos en proyectos
-			relacionados a una problemática que identificamos en la universidad, aunque también apoyamos
-			otros que sean de interés.
-		</p>
-		<p>
-			Puedes ver los proyectos en <a href="https://github.com/open-source-uc">nuestro GitHub</a> y
-			se participa cada semestre en la
-			<a href="/join">página de como unirse</a>.
-		</p>
-
 		<h2>Historia</h2>
 		<p>
-			Desde el 2006 existieron grupos como OpenPUC que buscaban incentivar el interés por el código
-			abierto en la universidad. El 2017 se armó un grupo que creó Open Source UC que se mantuvo
-			activo hasta finales del 2018. Y el 2021 se reorganizó la iniciativa, quedando en la
-			organización que conocemos ahora.
+			Desde el año 2006, existieron grupos como OpenPUC que buscaban incentivar el interés por el código
+			abierto en la universidad. En el año 2017, un grupo de estudiantes – inspirados en OpenPUC – creó Open Source UC, organización que se mantuvo
+			activa hasta finales del 2018. Desde el año 2021 se reorganizó la iniciativa, quedando en la
+			comunidad que conocemos ahora.
 		</p>
 
 		<h2>Timeline de Coordinadores</h2>
