d873abbb9c0c3ecee83eac5c6f4aaa1c4a306456
PabloCruzval <p.cruzat.valenzuela@gmail.com>
2025-11-13 23:04:41 -0300

    Update icon to new branding

--- src/routes/conduct/+page.svelte
+++ src/routes/conduct/+page.svelte
@@ -5,7 +5,7 @@
 			<p>
 				Si eres víctima o presenciaste un incidente dentro de nuestra comunidad, puedes denunciar y
 				conseguir apoyo contactando a nuestro Coordinador de Comunidad e Inclusión
-				(ignacio.palma@uc.cl) o Coordinador General (vml@uc.cl)
+				(noemi.flores@uc.cl).
 			</p>
 		</div>
 
